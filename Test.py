d8a5d15c51b0efb96e2f678673d29dfb1d430f59
WorstNormal <vladislavgaev84037@gmail.com>
2024-08-03 16:43:00 +0700

    create move_set_create

--- Test.py
+++ Test.py
@@ -1,22 +1,10 @@
-import collections
+def f(lis, n):
+    global kis
+    lis = lis.copy()
+    lis.pop()
+    kis = lis
 
-dq = collections.deque()
-def find(a, b, vis=None):
-	if
-	if a == b:
-		return l1[b[0]][b[1]]
-	if vis is None:
-		vis = [[True, True, True], [True, True, True], [True, True, True]]
-	vis[a[0]][a[1]] = False
-	
-l = [[1, 1, 1, 1, 1],
-     [1, 0, 1, 0, 1],
-     [1, 0, 1, 0, 1],
-     [1, 0, 0, 0, 1],
-     [1, 1, 1, 1, 1]]
-
-l1 = [[0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0]]
-
-print(find([1, 1], [1, 3]))
+kis = []
+lisq = [1, 2, 3]
+lisq = f(lisq, 1)
+print(kis)
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pygame
 import random
 import time
-
+import sys
 
 
 def draw_line(screen, size):
     for i in range(0, size[1]+ 1, 50):
-        pygame.draw.line(screen, pygame.Color('white'), [0, i], [size[0], i], width=1)
+        pygame.draw.line(screen, (100,100,100), [0, i], [size[0], i], width=1)
     for i in range(0, size[0] + 1, 50):
-        pygame.draw.line(screen, pygame.Color('white'), [i, 0], [i, size[1]], width=1)
+        pygame.draw.line(screen, (100,100,100), [i, 0], [i, size[1]], width=1)
 
 def draw_snake(screen, list, width, height):
     for i in range(0, height * 50, 50):
@@ -20,12 +20,12 @@ def draw_snake(screen, list, width, height):
                                      (j + 49, i + 49), (j + 1, i + 49)]
                                     , 0)
                 case 1:
-                    pygame.draw.polygon(screen, pygame.Color("green"),
+                    pygame.draw.polygon(screen, (0,150,0),
                                     [(j + 1, i + 1), (j + 49, i + 1),
                                      (j + 49, i + 49), (j + 1, i + 49)]
                                     , 0)
                 case 2:
-                    pygame.draw.polygon(screen, pygame.Color("green"),
+                    pygame.draw.polygon(screen, (0,150,0),
                                         [(j + 1, i + 1), (j + 49, i + 1),
                                          (j + 49, i + 49), (j + 1, i + 49)]
                                         , 0)
@@ -34,37 +34,69 @@ def draw_snake(screen, list, width, height):
                                             , (j + 30, i + 30) , (j + 30, i + 20)]
                                         , 0)
                 case 3:
-                    pygame.draw.circle(screen, pygame.Color("red"), (j + 25, i + 25), 24)
-                
+                    pygame.draw.circle(screen, (155, 31, 42), (j + 25, i + 25), 24)
+
 def predict(playground, move_set, apple, head, size):
     min_step = [10000, [0, 0]]
     head_y, head_x = head[0], head[1]
-    apple_y, apple_x = apple[0], apple[1]
     size_x, size_y = size[0], size[1]
     for x in range(-1, 2, 1):
         for y in range(-1, 2, 1):
             if abs(x) ^ abs(y):
                 if 0 <= head_x + x < size_x and 0 <= head_y + y < size_y:
-                    #print(move_set[head_y + y][head_x + x],  move_set[head_y][head_x], x, y)
-                    if move_set[head_y + y][head_x + x] - move_set[head_y][head_x] == 1 or move_set[head_y + y][head_x + x] - move_set[head_y][head_x] == -11:
+                    if (move_set[head_y + y][head_x + x] - move_set[head_y][head_x] == 1 or
+                            move_set[head_y + y][head_x + x] - move_set[head_y][head_x] == (size[0] * size[1] - 1) * -1):
                         min_step = [0, [x, y]]
-                        #min(abs(move_set[apple_y][apple_x] - move_set[head_y + y][head_x + x]),
-                        # ((12 - move_set[head_y + y][head_x + x]) + move_set[apple_y][apple_x]))
-    print(min_step)
     return min_step[1]
+
+def move_set_create(move_set_local, n, coord, size):
+    global move_set_best, max_n, flag_create
+    n += 1
+    size_x, size_y = size[0], size[1]
+    coord_y, coord_x = coord[0], coord[1]
+    move_set_change = move_set_local[::]
+    if coord_y + coord_x == 1 and ((n == size_y * size_x + 1) or
+            (size_x % 2 == 1 and size_y % 2 == 1 and size_y * size_x == n)):
+        move_set_best = move_set_local.copy()
+        max_n = n
+        flag_create = False
+    for x in range(1, -2, -1):
+        for y in range(-1, 2, 1):
+            if abs(x) ^ abs(y):
+                if (0 <= coord_x + x < size_x and 0 <= coord_y + y < size_y and move_set_change[coord_y + y][coord_x + x] == 0
+                        and flag_create):
+                    move_set_change[coord_y + y][coord_x + x] = n
+                    move_set_create(move_set_change, n, [coord_y + y, coord_x + x], size)
+                    if flag_create:
+                        move_set_change[coord_y + y][coord_x + x] = 0
+
+
+
+
+
 
 
 if __name__=='__main__':
-    move_set = [[11, 12, 1, 2], [10, 7, 6, 3], [9, 8, 5, 4]]
     start_pos = [1, [0, 2]]
-    pygame.init()
-    width, height = 4, 3 #map(int, input().split())
-    timer = 1
-    size = (width * 50, height * 50)
+    width, height = 10, 10
+    timer = 0.01
+
+
     playground = []
     for i in range(height):
         playground.append([0] * width)
+    move_set = []
+    for i in range(height):
+        move_set.append([0] * width)
+    max_n = 0
+    move_set[0][0] = 1
+    flag_create = True
+    move_set_best = []
+    move_set_create(move_set, 1, [0, 0], [width, height])
+    print(move_set_best)
+    last_move = [0, 2]
     head_snake = [0, 2]
+    pygame.init()
     playground[0][0] = 1
     playground[0][1] = 1
     playground[0][2] = 2
@@ -73,10 +105,11 @@ if __name__=='__main__':
         apple_y, apple_x = random.randint(0, height - 1), random.randint(0, width - 1)
     playground[apple_y][apple_x] = 3
     list_snake = [[0, 2], [0, 1], [0, 0]]
-    screen = pygame.display.set_mode(size)
     running = True
     move_x = 1
     move_y = 0
+    size = (width * 50, height * 50)
+    screen = pygame.display.set_mode(size)
     draw_line(screen, size)
     draw_snake(screen, playground, width, height)
     step_time = time.time()
@@ -109,9 +142,9 @@ if __name__=='__main__':
                                 flag = False
                 case "auto":
                     move_x, move_y = predict(playground, move_set, [apple_y, apple_x], head_snake, [width, height])
-                    """for event in pygame.event.get():
+                    for event in pygame.event.get():
                         if event.type == pygame.QUIT:
-                            running = False"""
+                            running = False
                     flag = False
         if time.time() - step_time >= timer:
             flag = True
